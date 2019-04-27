1080da4fed4055039a7995d1d9550539724b3e1f
brettmadrid <brettmadrid9@gmail.com>
2019-04-26 17:39:34 -0700

    setup database

--- api/server.js
+++ api/server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const reviews = require('../reviews/reviewsModel.js');
 
 const server = express();
+const multer = require('multer');
+const cors = require('cors');
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', async (req, res) => {
   res.status(200).json({ message: 'I am Son of Hal and I am always watching!' });
